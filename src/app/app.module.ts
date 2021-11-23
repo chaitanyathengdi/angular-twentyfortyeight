54a42b3c4fd6d731b140f4e82ce98e09f43e519c
Chaitanya Thengdi <thengdi.chaitanya.sudhir@gmail.com>
2021-11-23 15:56:22 +0530

    Add barebones components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
+import { BoardComponent } from './board/board.component';
+import { TileComponent } from './tile/tile.component';
+import { InfoComponent } from './info/info.component';
+import { ActionsComponent } from './actions/actions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    BoardComponent,
+    TileComponent,
+    InfoComponent,
+    ActionsComponent
   ],
   imports: [
     BrowserModule
