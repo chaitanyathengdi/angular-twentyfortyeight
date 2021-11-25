0d7003483f135ff9ed7ae767b0c785386c38222e
Chaitanya Thengdi <thengdi.chaitanya.sudhir@gmail.com>
2021-11-25 12:39:48 +0530

    Add tile colors according to values

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -6,10 +6,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./board.component.css']
 })
 export class BoardComponent implements OnInit {
+  values = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  getClassForValue (value:number | null) {
+    type tClasses = {
+      [key: number]: string
+    }
+  
+    const classes:tClasses = {
+      2: "two",
+      4: "four",
+      8: "eight",
+      16: "sixteen",
+      32: "thirty-two",
+      64: "sixty-four",
+      128: "one-twenty-eight",
+      256: "two-fifty-six",
+      512: "five-one-two",
+      1024: "one-k",
+      2048: "two-k",
+      4096: "four-k",
+      8192: "eight-k",
+      16384: "sixteen-k",
+      32768: "thirty-two-k",
+      65536: "sixty-five-k",
+      131072: "one-thirty-one-k"
+    };
+
+    const returnValue = value ? classes[value] : 'blank';
+    return returnValue;
   }
 
 }
--- src/app/tile/tile.component.ts
+++ src/app/tile/tile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'tile',
@@ -7,9 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TileComponent implements OnInit {
 
-  constructor() { }
+  selectedClass:string = "";
+
+  @Input() value!:number|null;
+  constructor() {
+  }
 
   ngOnInit(): void {
+    type tClasses = {
+      [key: number]: string
+    }
+  
+    const classes:tClasses = {
+      2: "two",
+      4: "four",
+      8: "eight",
+      16: "sixteen",
+      32: "thirty-two",
+      64: "sixty-four",
+      128: "one-twenty-eight",
+      256: "two-fifty-six",
+      512: "five-one-two",
+      1024: "one-k",
+      2048: "two-k",
+      4096: "four-k",
+      8192: "eight-k",
+      16384: "sixteen-k",
+      32768: "thirty-two-k",
+      65536: "sixty-five-k",
+      131072: "one-thirty-one-k"
+    };
+
+    this.selectedClass = this.value ? classes[this.value] : "blank";
   }
 
 }
